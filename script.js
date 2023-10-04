b22dd9e89b2bdb7001b584bcd5666b51d7ba9305
MAT-2 <mpcasey30@gmail.com>
2023-10-04 16:37:35 -0500

    Added addtional function to script.js to see if another way to have the time to interval can work.

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 var today = dayjs();
-$("#timeCurrent").text(today.format("MMM, DD, YYYY, hh:mm:ss "));
+$("#timeCurrent").text(today.format("MMM DD, YYYY, hh:mm:ss a"));
+
+//Need to use a time.Interval function to display the date and time every second, updating!
+
+var time = $("#displayTime");
+
+function displayTime() {
+  var timeNow = dayjs().format("MMM DD, YYYY, hh:mm:ss a");
+  today.text(timeNow);
+}
+
+setInterval(today, 1000);
+
+displayTime();
